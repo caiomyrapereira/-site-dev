99ea75534d48ac684d4c0993b8c1305eea64f505
caio myra pereira <caiomyrapereira@hotmail.com>
2019-09-01 23:06:01 -0300

    add meus arquivos

---
+++ js/app.js
@@ -0,0 +1,25 @@
+(function(wind, doc) {
+
+
+    'use strict';
+
+    function on(element, event, callback) {
+        doc.querySelector(element)
+            .addEventListener(event, callback, false)
+    }
+    on('[datajs="active"]', 'click', function(event) {
+        alert('ok')
+    })
+
+    doc.addEventListener('DOMContentLoaded', function() {
+        var elems = doc.querySelectorAll('.parallax');
+        var instances = M.Parallax.init(elems);
+    });
+
+
+
+
+
+
+
+})(window, document)
