19d1b74f99449a8e896aa3fe3efba7cf7be62bd0
caio myra pereira <caiomyrapereira@hotmail.com>
2019-10-22 15:08:13 -0300

    add event closeSadmenu

--- js/app.js
+++ js/app.js
@@ -3,28 +3,42 @@
 
     'use strict';
 
-    function on(element, event, callback) {
-        doc.querySelector(element)
-            .addEventListener(event, callback, false)
-    }
-    on('[datajs="active"]', 'click', function(event) {
-        alert('ok')
-    })
 
-    doc.addEventListener('DOMContentLoaded', function() {
-        var elems = doc.querySelectorAll('.parallax');
-        var instances = M.Parallax.init(elems);
-    });
+    const app = (function() {
+        return {
+            initialize: function() {
+                this.initEvent();
+            },
+            initEvent: function() {
+                this.parallax()
+                this.initSadMenu()
+                this.closeSadMenu()
 
-    doc.addEventListener('DOMContentLoaded', function() {
-        var elems = doc.querySelectorAll('.sidenav');
-        var instances = M.Sidenav.init(elems);
-    });
+            },
+            parallax: function() {
+                doc.addEventListener('DOMContentLoaded', function() {
+                    const elems = doc.querySelectorAll('.parallax');
+                    const instances = M.Parallax.init(elems);
+                });
+            },
+            initSadMenu: function() {
+                doc.addEventListener('DOMContentLoaded', function() {
+                    const elems = doc.querySelectorAll('.sidenav');
+                    const instances = M.Sidenav.init(elems);
+                });
+            },
+            closeSadMenu: function() {
+                const elem = document.querySelector('.sidenav');
+                const $buttons = doc.querySelectorAll('[data-js="buttonSadmenu"]');
+                Array.prototype.forEach.call($buttons, function(button) {
+                    button.addEventListener('click', function(e) {
+                        $('.sidenav').sidenav('close');
+                    }, false)
+                })
+            }
+        };
+    })()
 
-
-
-
-
-
+    app.initialize()
 
 })(window, document)
